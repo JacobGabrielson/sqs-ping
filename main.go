6e5eefd8ec16f6236a200d123166966d87d0975a
jacob <jacob@amazon.com>
2021-03-02 15:37:42 -0800

    qux

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
-	_ "github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
 func send(ctx context.Context, client *sqs.Client, queueURL fmt.Stringer, reader io.Reader) error {
